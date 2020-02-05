53ab08f888e8df0525c663dea6126f6a9f8bd536
victoriahao <mwei5669@gmail.com>
2020-02-04 22:59:52 -0800

    week 4 assignment completed

---
+++ src/data/articles.jsx
@@ -0,0 +1,173 @@
+import React from 'react'
+
+const Article = () => {
+    return (
+         <div>
+              <ul>
+                  <li>
+                      <p><h3>The Statue of Liberty's torch heads to new museum</h3></p>
+                  </li>
+              </ul>
+                      <p>The Statue of Liberty's original torch is getting a new home</p>
+                      <p>Thu, 22 Nov 2018 15:12:24 GMT</p>
+              
+  
+              <ul>
+                  <li>
+                      <p><h2>You can take a pie on a plane</h2></p>
+                  </li>
+              </ul>
+                      <p>Getting excited to eat your mother's roast turkey, your uncle's stuffing and your sister's famous green bean casserole?</p>
+                      <p>Wed, 21 Nov 2018 22:57:55 GMT</p>
+
+
+  
+              "title": "Butter is back, just in time for the holidays",
+              "shortText": "Amid the farm houses and barns dotting the Mad River Valley area of rural Vermont, five miles north of Sugarbush ski resort, Marisa Mauro is making butter.",
+              "pubDate": "Wed, 21 Nov 2018 13:17:49 GMT",
+    
+  ,
+
+    "title": "Boeing's 777X jetliner comes together ",
+    "shortText": "Travelers around the globe are one step closer to seeing a plane with its wings folded up being taxied to their gate.",
+    "pubDate": "Thu, 22 Nov 2018 14:58:47 GMT",
+   
+  ,
+  
+    "title": "Leaning Tower of Pisa loses some of its tilt",
+    "shortText": "The Leaning Tower of Pisa has slowly started defying its name, losing four centimeters of its tilt over the past 17 years.",
+    "pubDate": "Thu, 22 Nov 2018 17:55:57 GMT",
+    
+  ,
+  
+    "title": "Training India's next generation of female pilots",
+    "shortText": "On a dusty, 3,600-foot-long airstrip in Rajasthan, India, a two-seater white Cessna 152 pivots into position. A few moments later, it's soaring over the empty desert into a cloudless blue sky.",
+    "pubDate": "Thu, 22 Nov 2018 02:43:51 GMT",
+  ,
+  
+    "title": "Ancient termite megapolis as large as Britain found ",
+    "shortText": "Researchers have discovered a gigantic complex of 200 million termite mounds in northeastern Brazil, which may be up to 4,000 years old and covers an area about the size of Great Britain.",
+    "pubDate": "Wed, 21 Nov 2018 12:19:45 GMT",
+
+  ,
+ 
+    "title": "Travel deals for Black Friday and Cyber Monday ",
+    "shortText": "Sure, Thanksgiving is a time to give thanks. But the extended holiday weekend also is a time for some serious retail therapy, especially on discounted travel.",
+    "pubDate": "Fri, 23 Nov 2018 01:29:06 GMT",
+
+  ,
+  
+    "title": "This Alaskan town won't see the sun for 65 days",
+    "shortText": "If you're a night owl, you may want to consider spending your winters in northern Alaska.",
+    "pubDate": "Tue, 20 Nov 2018 21:37:11 GMT",
+
+  ,
+  
+    "title": "Airline etiquette: Tips from an expert ",
+    "shortText": "I was sitting in an aisle seat on a flight earlier this year when a fellow traveler approached and politely said he thought I may be sitting in his seat.",
+    "pubDate": "Mon, 19 Nov 2018 11:10:20 GMT",
+
+  ,
+  
+    "title": "India's first woman with a Michelin star",
+    "shortText": "Chef Garima Arora just became the first Indian woman to win a Michelin star.",
+    "pubDate": "Fri, 16 Nov 2018 11:07:32 GMT",
+
+  ,
+  
+    "title": "New Zealand asks travelers to pledge a 'Tiaki Promise' ",
+    "shortText": "New Zealand is known for its strict environmental rules and regulations.",
+    "pubDate": "Sat, 17 Nov 2018 00:57:34 GMT",
+    
+  ,
+  
+    "title": "Eat your way through Mexico City's largest market",
+    "shortText": "Mexico City's love affair with food is all-encompassing, and no trip is complete without a thorough food tour of Mercado de la Merced, the city's largest market and one of its oldest.",
+    "pubDate": "Thu, 15 Nov 2018 04:10:21 GMT",
+
+  ,
+  
+    "title": "Go glamping with Hello Kitty in Japan",
+    "shortText": "Hello Kitty is one of Japan's most popular -- and definitely most adorable -- exports.",
+    "pubDate": "Mon, 19 Nov 2018 01:16:53 GMT",
+    
+  ,
+  
+    "title": "World's 'most excellent' airline named",
+    "shortText": "Air New Zealand gets regular kudos for innovating everything from in-flight services to safety videos, its funky films featuring Hobbits and other famous Kiwis.",
+    "pubDate": "Mon, 06 Nov 2017 10:40:40 GMT",
+
+  ,
+  
+    "title": "Why travelers are crashing Indian weddings",
+    "shortText": "Always fantasized about attending a big blow-out India wedding, complete with days of celebration, delicious food, vibrant interiors and spectacular clothing?",
+    "pubDate": "Wed, 14 Nov 2018 05:36:18 GMT",
+    
+  ,
+  
+    "title": "Ski down the roof of this new Swiss hotel",
+    "shortText": "Until now, skiing off hotel roofs has been an exploit reserved for James Bond stunt doubles and drunken fools with a penchant for plaster casts.",
+    "pubDate": "Wed, 14 Nov 2018 12:20:29 GMT",
+  ,
+  
+    "title": "Bangkok's new $1.65 billion mega attraction",
+    "shortText": "Pack your travel bags, grab your credit cards.",
+    "pubDate": "Wed, 14 Nov 2018 02:28:48 GMT",
+    
+  ,
+  
+    "title": "Cliff-jumping off Ireland's Atlantic coast",
+    "shortText": "If someone told you to jump into an often tumultuous and always frigid North Atlantic Ocean from a 30-foot-high cliff in Ireland, would you do it?",
+    "pubDate": "Tue, 13 Nov 2018 09:55:34 GMT",
+    
+  ,
+  
+    "title": "Why some women won't eat out alone ",
+    "shortText": "Taking myself out to a local Japanese restaurant for dinner shouldn't have been a big deal, but it became one before I even picked up my chopsticks.",
+    "pubDate": "Thu, 15 Nov 2018 16:01:15 GMT",
+    
+  ,
+ 
+    "title": "The coldest Thanksgiving in over a century for millions",
+    "shortText": "Tranquil weather will accompany most Americans heading to a loved one's dinner table for Thanksgiving. But they might feel a little heartburn on the return home Saturday and Sunday, given the traffic congestion and dicey conditions in the forecast. Check out the forecast for the long Thanksgiving holiday.",
+    "pubDate": "Tue, 13 Nov 2018 09:55:34 GMT",
+    
+  ,
+  
+    "title": "New Zealand's North and South Islands moving closer together",
+    "shortText": "New Zealand's North and South Islands are moving closer together after a series of earthquakes in 2016 that lifted the sea floor, the country's geology body has said.",
+    "pubDate": "Tue, 20 Nov 2018 09:55:34 GMT",
+
+  ,
+  
+    "title": "25 gifts travelers will actually want",
+    "shortText": "The true traveler wants things that are either necessary or chic -- or both. So check out our guide of 25 fantastic gifts for every traveler on your list.",
+    "pubDate": "Tue, 20 Nov 2018 09:55:34 GMT",
+    
+  ,
+  
+    "title": "31 beautiful reasons to visit England",
+    "shortText": "Discover England's most beautiful places. From the wild North York Moors to the ancient Roman sites in Bath, you be enchanted by scenes natural and man-made.",
+    "pubDate": "Thu, 22 Nov 2018 09:55:34 GMT",
+
+  ,
+  
+    "title": "The 32 most beautiful places to visit in Ireland",
+    "shortText": "From the dramatic Cliffs of Moher to regal Powerscourt Estate in Wicklow, escape to 32 of the most beautiful places to visit in Ireland.",
+    "pubDate": "Thu, 22 Nov 2018 09:55:34 GMT",
+    
+  ,
+  
+    "title": "30 of India's most beautiful places",
+    "shortText": "India is a land of bountiful landscapes from stunning lakes, rolling plains and the snow-capped Himalayas to buildings of worldwide acclaim. See 30 of India's most beautiful places.",
+    "pubDate": "Tue, 27 Nov 2018 09:55:34 GMT",
+    
+
+
+
+      </div>
+    )
+}
+
+export default Article
+
